134115688d14358e2e3f18b15e7d2f2019f56819
Abdullah <abdullahaziz723@gmail.com>
2025-04-25 22:18:50 +0530

    sda

--- chapter_3/string.py
+++ chapter_3/string.py
@@ -6,3 +6,6 @@ print(ext)
 print (name[-4:-1])  #==change to coresponding values =1,4
 print(name[:4])  #start from -1 
 print(name[1:])
+
+str="the quick brown fox jumps over the lazy  dog"
+print(str.replace("quick","fox"))
