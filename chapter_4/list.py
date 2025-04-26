6a40013fcb67981879267833a2d8e4202fbfc078
Abdullah <abdullahaziz723@gmail.com>
2025-04-26 11:17:51 +0530

    methods

---
+++ chapter_4/list.py
@@ -0,0 +1,4 @@
+#type  of array in python
+
+friends=["apple","car","banana","orange",5,231,False]
+print(friends) #apple  #unlike list are mutable in nature ,we can change the values of list,wheareas string are immutable in nature
---
+++ chapter_4/list_methods.py
@@ -0,0 +1,14 @@
+friends=["apple","car","banana","orange",5,231,False]
+print(friends)
+
+friends.append("mango") #append method is used to add the element at the end of the list
+print(friends) 
+
+l1=[213,3213,66,11,77,88]
+l1.insert(2,100)
+print(l1) #insert method is used to insert the element at the specified index
+l1.remove(100) #remove method is used to remove the element from the list
+print(l1) #remove method is used to remove the element from the list
+l1.pop(2) #pop method is used to remove the element from the specified index    
+print(l1) #pop method is used to remove the element from the specified index
+
