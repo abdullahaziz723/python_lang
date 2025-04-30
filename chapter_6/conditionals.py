86faf987059de61b863573f569cc8aee8e9601cc
Abdullah <abdullahaziz723@gmail.com>
2025-04-30 22:15:25 +0530

    qr

---
+++ chapter_6/conditionals.py
@@ -0,0 +1,7 @@
+a= int( input("enter a number")) 
+if(a>18):
+    print("you are a adult")
+else:
+    print("you are not a adult")
+# The above code is a simple conditional statement in Python that checks if a number entered by the user is greater than 18.
+
---
+++ qr.py
@@ -0,0 +1,9 @@
+import qrcode
+
+# Data to encode
+data =qrcode.make("https://github.com/abdullahaziz723") 
+
+data.show()
+
+
+
