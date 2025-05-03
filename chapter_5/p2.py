5322216c98d87a980e6af0fb3376c4835191d856
Abdullah <abdullahaziz723@gmail.com>
2025-05-03 23:58:35 +0530

    aa

--- chapter_5/p2.py
+++ chapter_5/p2.py
@@ -1,4 +1,19 @@
 s={
     12,'harry'
 }
-print(s)
+# print(s)
+
+s=set()
+s.add(12)
+s.add('harry')
+s.add("abd")
+s.add(12.5)
+s.add(12)   # set does not allow duplicates
+s.add((1,2,3))  # set allows tuples
+# print(s)
+S=set()
+S.add(12)
+S.add('harry')
+S.add(12.0)
+print(S)
+print(len(S))
