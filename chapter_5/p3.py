d17cc610c63d1dba9e573f9e920b161634f8f4cf
Abdullah <abdullahaziz723@gmail.com>
2025-05-04 22:57:09 +0530

    bb

--- chapter_5/p3.py
+++ chapter_5/p3.py
@@ -1,5 +1,6 @@
 #the values entered 
 s={8,9,12,"harry",[1,2]}
+s=(7,8,9,12,"harry",[1,2])
 
 # print(s)#it will give error because list is not hashable
 #it will give error because list is not hashable
