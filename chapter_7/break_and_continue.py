8ae68838b9cded3367d19c93e5d9f58ef13d5bb0
Abdullah <abdullahaziz723@gmail.com>
2025-05-07 16:06:31 +0530

    concepts_done

---
+++ chapter_7/break_and_continue.py
@@ -0,0 +1,12 @@
+
+for i in range (100):
+    if i == 50:
+        break
+    print(i)  #exit the loop when i is 50
+# for loop with continue statement 
+
+
+for j in range(100):
+    if j ==50:
+        continue #skip the iteration
+    print(j)    
---
+++ chapter_7/for_loop_with_else.py
@@ -0,0 +1,8 @@
+l=[1,4,6]
+
+for item  in l:
+    print(item)
+
+
+else:
+    print("for loop ended")
