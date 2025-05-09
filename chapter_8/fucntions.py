9fd239c4ada8cfde1859374ba06b76e97990b48a
Abdullah <abdullahaziz723@gmail.com>
2025-05-09 16:56:26 +0530

    chapter_8

---
+++ chapter_8/fucntions.py
@@ -0,0 +1,18 @@
+
+
+
+def avg():
+    a=int(input("Enter a number: "))
+    b=int(input("Enter a number: "))
+    c=int(input("Enter a number: "))
+
+
+
+    avge=(a+b+c)/3
+    print(avge)
+
+
+def greet(name):
+    print("hello "+ name)
+    
+greet("sachin")    
