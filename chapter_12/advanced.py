082a4d1c44eb44dca3c955fc08a15a622d5433e4
abdullah <abdullahaziz723@gmail.com>
2025-06-22 00:38:49 +0530

    python

---
+++ chapter_12/advanced.py
@@ -0,0 +1,7 @@
+#walrus opeartor    
+if(n:=len([1,2,3,4,5,6,7,8,9,10])) > 5:
+    print(f"list is long {n} elements,expected less than 5")
+
+
+
+    # type definations
--- qr.py
+++ qr.py
@@ -1,19 +1,19 @@
 import qrcode
-# Data to encode
-data =qrcode.make("https://polite-queijadas-0aa65d.netlify.app/") 
 
-data.show()
-
-
-
-data = ['abdillah', 'aziz', '723']
-# Create QR Code
+# 1. Encode a simple text message
+message = "Hello pipi yeh lo QR code! pglu"
 qr = qrcode.QRCode(
     version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    # error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
-qr.add_data(data)
+qr.add_data(message)
 qr.make(fit=True)
 
+# 2. Create an image from the QR code
+img = qr.make_image(fill_color="blue", back_color="white")
+
+# 3. Show or save the QR code
+img.show()               # Shows the QR code
+img.save("my_qr.png")    # Saves the QR code as an image
